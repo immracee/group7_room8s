9396574574a878dce71af221c87a0a90e7cf6301
Elena <42975350+immracee@users.noreply.github.com>
2019-03-29 21:02:30 -0400

    Begin work on loginpage
    Doing intital work getting login functionality to work.

---
+++ process-login.php
@@ -0,0 +1,19 @@
+<?php
+session_start();
+require_once('database.php');
+$username = $_POST['loginUsername'];
+$password = $_POST['loginPassword'];
+
+
+$stmt = $pdo->prepare("SELECT * FROM `signupRoommate` WHERE `email` = '$email' AND `password` = '$password'");
+
+$stmt->execute();
+
+if($row = $stmt->fetch()){
+	$_SESSION['logged-in'] = true;
+	$_SESSION['email'] = $row['email'];
+	header("Location: loggedin.php");
+}else{
+	header("Location: loginfail.php");
+}
+?>

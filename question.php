1cd13541f6538446e6f2adb182aac92730bb9c68
Shiqi57 <42975365+Shiqi57@users.noreply.github.com>
2019-03-31 00:26:04 -0400

    Update questionnaire
    Insert answer to database, haven't test it in server

---
+++ question.php
@@ -0,0 +1,20 @@
+<?php
+require_once('database.php');
+
+$question1 ='';
+$question2 ='';
+$question3='';
+
+
+  $question1 = $_POST['question1'];
+  $question2 = $_POST['question2'];
+  $question3 = $_POST['question3'];
+  $email = $_POST['email'];
+
+// $stmt = $pdo->prepare("INSERT INTO `signupRoommate` (`firstName`, `lastName`, `phone`, `email`, `city`, `password`) VALUES ('$firstName', '$lastName', '$phone', '$email', '$city', '$password'); ");
+$stmt = $pdo->prepare("UPDATE `signupRoommate` SET `question1` = '$question1', `question2` = '$question2', `question3` = '$question3' WHERE `signupRoommate`.`email` = $email;");
+
+$stmt->execute();
+
+// header("Location: registrationreceived.php");
+?>

c6c4fd319f083cc52470af2266b750a0a227849e
Ikki <kaatkreative@gmail.com>
2019-04-10 01:44:22 -0400

    Added Map API & JS
    Adding the Map api and js folder. Refinement coming.

---
+++ js/scripts.js
@@ -0,0 +1,107 @@
+var locationSend = document.getElementById('submitLocation');
+locationSend.addEventListener("click", getLocation);
+var searchLocation = "toronto";
+var locations = [];
+
+function getLocation(e){
+  searchLocation = document.getElementById('location').value;
+  loadLocations();
+  setMapCenter();
+}
+
+function loadLocations(){
+  switch (searchLocation.toLowerCase(searchLocation)){
+    case 'kitchener':
+      locations = [
+        {
+          name: 'Trio on Belmont',
+          Bdrms: 2,
+          Bath: 1.5,
+          Price: '$1700',
+          lat:43.446410,
+          lng:-80.513170
+        }
+      ];
+      break;
+    case 'toronto':
+      locations = [];
+      break;
+    default:
+      locations = [];
+      break;
+  }
+}
+
+// Initialize the platform object:
+var platform = new H.service.Platform({
+app_id: 'EgttRMYsxwEkogtDpzlQ',
+app_code: 'PxKgB0IYXUVfkVklFw-7dA'
+});
+
+// Obtain the default map types from the platform object
+var defaultLayers = platform.createDefaultLayers();
+
+// Create the parameters for the geocoding request:
+var geocodingParams = {
+    searchText: searchLocation
+};
+
+var map = new H.Map(
+    document.getElementById('mapContainer'),
+    defaultLayers.normal.map,
+    {
+        zoom: 12,
+        center: { lat: 52.51, lng: 13.4 }
+    });
+
+// Create the default UI:
+var ui = H.ui.UI.createDefault(map, defaultLayers);
+var mapSettings = ui.getControl('mapsettings');
+var zoom = ui.getControl('zoom');
+var scalebar = ui.getControl('scalebar');
+mapSettings.setAlignment('top-left');
+zoom.setAlignment('top-left');
+scalebar.setAlignment('top-left');
+
+// Define a callback function to process the geocoding response:
+var onResult = function(result) {
+    var locations = result.Response.View[0].Result,
+    position,
+    marker;
+    console.log(locations);
+    map.setCenter({
+        lat: locations[0].Location.DisplayPosition.Latitude,
+        lng: locations[0].Location.DisplayPosition.Longitude},
+        true);
+    setDummyLocations();
+};
+
+// Get an instance of the geocoding service:
+var geocoder = platform.getGeocodingService();
+geocoder.geocode(geocodingParams, onResult, function(e) {
+    alert(e);
+});
+
+function setMapCenter(){
+    //get the written address from some field
+    geocodingParams = { searchText: searchLocation};
+    geocoder.geocode(geocodingParams, onResult, function(e) {
+    alert(e);
+});
+}
+
+// Define a callback function to process the geocoding response:
+function setDummyLocations() {
+  // Add a marker for each location found
+  for (i = 0;  i < locations.length; i++) {
+  position = {
+    lat: locations[i].lat,
+    lng: locations[i].lng
+  };
+  marker = new H.map.Marker(position);
+  map.addObject(marker);
+  }
+};
+
+//This guide explains how to get stuff from addressess and add them as map markers (just fake some data)
+    //https://developer.here.com/documentation/maps/topics/geocoding.html
---
+++ rentalapi.php
@@ -0,0 +1,26 @@
+
+<html>
+    <head>
+        <meta name="viewport" content="initial-scale=1.0,
+          width=device-width" />
+        <script src="http://js.api.here.com/v3/3.0/mapsjs-core.js"
+          type="text/javascript" charset="utf-8"></script>
+        <script src="http://js.api.here.com/v3/3.0/mapsjs-service.js"
+          type="text/javascript" charset="utf-8"></script>
+        <script src="http://js.api.here.com/v3/3.0/mapsjs-ui.js"
+          type="text/javascript" charset="utf-8"></script>
+        <link rel="stylesheet" type="text/css"
+          href="http://js.api.here.com/v3/3.0/mapsjs-ui.css" />
+      </head>
+  <body>
+
+
+      <input type="text" name="location" id="location">
+      <button type="submit" name="button" id="submitLocation">submit</button>
+      <br/> <br/>
+
+    <div style="width: 640px; height: 480px" id="mapContainer"></div>
+
+  <script src="./js/scripts.js"></script>
+  </body>
+</html>
